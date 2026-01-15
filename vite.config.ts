21c08fe196f96b4a0583c112fb02f0119ce14187
Zulfikharali Muzakir <zul26muzakir@gmail.com>
2026-01-16 00:38:31 +0700

    Revert "chore(build): remove cloudflare workers setup, dependencies, and config"
    This reverts commit c2d2beb8f0b830a73102773c5055299c8151993b.

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import netlifyReactRouter from "@netlify/vite-plugin-react-router"
+import {cloudflare} from "@cloudflare/vite-plugin"
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
-    netlifyReactRouter({ edge: true }),
+    cloudflare(
+      {
+        viteEnvironment: {name: "ssr"}
+      }
+    ),
+    reactRouter(),
   ],
 });
