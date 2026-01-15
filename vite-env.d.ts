63d53099470006e93e10b629924ae53482b4be1f
Zulfikharali Muzakir <zul26muzakir@gmail.com>
2026-01-15 20:55:43 +0700

    feat(netlify): add edge functions support
    Integrates `@netlify/vite-plugin-react-router` to enable
    server-side rendering on Netlify Edge Functions.

---
+++ vite-env.d.ts
@@ -0,0 +1,5 @@
+declare module 'virtual:netlify-server-entry' {
+  import type { ServerEntryModule } from 'react-router'
+  const entry: ServerEntryModule
+  export default entry
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import netlifyReactRouter from "@netlify/vite-plugin-react-router"
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
+    netlifyReactRouter({ edge: true }),
   ],
 });
