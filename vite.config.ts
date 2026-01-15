84564994f7d16916cfa32680b84369f2d2c7b759
Zulfikharali Muzakir <zul26muzakir@gmail.com>
2026-01-15 20:15:30 +0700

    feat(cloudflare): integrate worker for SSR
    Add `app/entry.server.tsx` for React Router server-side rendering. Configure
    `vite.config.ts` with `@cloudflare/vite-plugin` and introduce `wrangler.jsonc`
    and `workers/app.ts` as the Cloudflare Worker entry point. Update `package.json`
    scripts for `deploy`, `preview`, and `typecheck`.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import {cloudflare} from "@cloudflare/vite-plugin"
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
+    cloudflare(
+      {
+        viteEnvironment: {name: "ssr"}
+      }
+    ),
+    reactRouter(),
   ],
 });
